99af82ac95e4281f48dc719d4ec5722a5dec83bb
Sen-Yang2005 <2961134Y@student.gla.ac.uk>
2024-04-13 14:35:57 +0100

    make an initial comment to clear the arguments and return value.

--- Euclidean Algorithms.py
+++ Euclidean Algorithms.py
@@ -1,1 +1,11 @@
 class EuclideanAlgorithms:
+    def GCD(self,a,b):
+        """calculate the GCD of given two positive integers by using Euclidean Algorithm
+           args:
+           a : positive integer
+           b : positive integer
+
+           returns:
+           GCD of given two positive integers
+        """
+
